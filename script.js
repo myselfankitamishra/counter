c07b7a1cc4e1d9f7d1f0f439498da480ff6cc7ad
Anton Shekhov <shekhov.anton@gmail.com>
2024-04-29 20:43:12 +0200

    Great job!

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Very good job on the script, there is not much I would chage myself.
+// I like that you are using a common function updateCounterDisplay, while its not actually
+// required for such task, its a good practice to think about what could be moved away into 
+// a separate function.
+// 
+// While it is a fine practice to use alerts for a debugging purposes, it is rarely used in the 
+// web development. If you want, you can programatically change some of your html to communicate
+// that there is a upper limit and the user has reached it. 
+// Starting with adding a text element and changing the color upon reaching the limit,
+// to actually disabling the button when the counter hits upperLimit.
 let counterValue = 0;
 const upperLimit = 30;
 const counterDisplay = document.getElementById("counter");
