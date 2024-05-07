4a1d9c61befe9194924158cf403a09a5767c6c7a
myselfankitamishra <ankitaarmishra@gmail.com>
2024-05-07 17:43:52 +0200

    updated js

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
 let counterValue = 0;
 const upperLimit = 30;
-const counterDisplay = document.getElementById("counter");
-
-function increase() {
-    if(counterValue < upperLimit){
-    counterValue++;
-    updateCounterDisplay();}
-    else{
-        alert("Hey! You reach the upper upper limit.")
+let counterDisplay = document.getElementById("counter")
+ 
+function increase(){
+   
+    if(counterValue<upperLimit){
+         counterValue++
+          updateCounterDispay()
+    }else{
+        return alert(`You have reached the Upper Limit.`)
     }
+   spanDisplay()
 }
-
-function decrease() {
-    if (counterValue > 0) {
+function decrease(){
+    if(counterValue>0){
         counterValue--;
-        updateCounterDisplay();
+        updateCounterDispay();
     }
+    spanDisplay()
 }
-
-function reset() {
+function reset(){
     counterValue = 0;
-    updateCounterDisplay();
+    newSpan.style.visibility = "hidden";
+    plusButton.style.backgroundColor = "burlywood";
+    return updateCounterDispay();
+}
+function updateCounterDispay(){
+    counterDisplay.textContent=counterValue;
+    console.log(counterValue);
 }
 
-function updateCounterDisplay() {
-    counterDisplay.textContent = counterValue;
+const newSpan = document.createElement("span");
+newSpan.textContent = "You have free shipping";
+newSpan.style.width = "200px";
+newSpan.style.visibility = "hidden";
+newSpan.style.display = "flex";
+newSpan.style.justifyContent = "center";
+newSpan.style.margin = "auto";
+newSpan.style.marginTop = "10px";
+newSpan.style.backgroundColor = "olive";
+newSpan.style.color = "white";
+document.body.appendChild(newSpan);
+
+const plusButton = document.getElementById("plus");
+function spanDisplay(){
+if(counterValue>=20){
+newSpan.textContent = "Out of Stock";
+plusButton.style.backgroundColor = "red";
+}else if(counterValue > 9 && counterValue < 20){
+    // console.log("hello");
+    newSpan.style.visibility = "visible";
+    newSpan.textContent = "You have free shipping";
+    plusButton.style.backgroundColor = "burlywood";}
+else{
+    
+    newSpan.style.visibility = "hidden";
+}
 }
